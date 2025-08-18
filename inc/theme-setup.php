6a035135f8c82902c7b99b8645260118188a36ba
kskep <130034485+kskep@users.noreply.github.com>
2025-08-18 14:05:58 +0300

    test gemini

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -83,15 +83,6 @@ add_action('after_setup_theme', 'eshop_theme_gutenberg_colors');
  * Register Widget Areas
  */
 function eshop_theme_widgets_init() {
-    register_sidebar(array(
-        'name' => __('Sidebar', 'eshop-theme'),
-        'id' => 'sidebar-1',
-        'description' => __('Add widgets here.', 'eshop-theme'),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget' => '</section>',
-        'before_title' => '<h2 class="widget-title">',
-        'after_title' => '</h2>',
-    ));
     
     register_sidebar(array(
         'name' => __('Footer Widget Area', 'eshop-theme'),
