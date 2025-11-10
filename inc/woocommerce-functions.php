10b3a4d9bbce73dc660647eadcbdca9bf0d22fde
kskep <130034485+kskep@users.noreply.github.com>
2025-11-11 01:01:12 +0200

    fy

--- inc/woocommerce-functions.php
+++ inc/woocommerce-functions.php
@@ -298,7 +298,9 @@ function eshop_dequeue_woocommerce_styles() {
 }
 add_action('wp_enqueue_scripts', 'eshop_dequeue_woocommerce_styles', 100);
 
-// Remove default WooCommerce sale flash (we handle it in product-card.php)
+// Remove default WooCommerce sale flash actions (we handle it in product-card.php)
+remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10);
+remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10);
 add_filter('woocommerce_sale_flash', '__return_empty_string', 10, 3);
 
 /**
